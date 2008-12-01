f5e7b7450d806618cf7dff9f2c8ff77fb23b3ad8
Nicolas Jacobeus <nj@belighted.com>
2008-12-01 15:09:05 +0100

    put back tokboxer.rb

---
+++ lib/tokboxer.rb
@@ -0,0 +1,28 @@
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+require 'rubygems'
+require 'net/http'
+require 'uri'
+require 'digest/md5'
+require 'cgi'
+require 'xmlsimple'
+require 'pp' # just for debugging purposes
+
+module TokBoxer
+  VERSION = '0.1.3'
+  
+  API_SERVER_LOGIN_URL       = "view/oauth&"
+  API_SERVER_METHODS_URL     = "a/v0"
+  API_SERVER_CALL_WIDGET     = "vc/"
+  API_SERVER_RECORDER_WIDGET = "vr/"
+  API_SERVER_PLAYER_WIDGET   = "vp/"
+  API_VERSION                = "1.0.0"
+  API_SIGNATURE_METHOD       = "SIMPLE-MD5"
+end
+
+require 'TokBoxer/Api'
+require 'TokBoxer/User'
+require 'TokBoxer/Call'
+require 'TokBoxer/VMail'
+require 'TokBoxer/Exceptions'
